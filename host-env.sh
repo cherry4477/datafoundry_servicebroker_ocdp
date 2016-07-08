48bfb9d8a421f020f7e50cafcb624000129a9684
MySQL <871810908@qq.com>
2016-07-08 11:20:09 +0800

    Update host-env.sh

--- host-env.sh
+++ host-env.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-echo $HOST-1                 
-echo $HOST-1> /etc/hosts
-echo $HOST-1> /etc/hosts
-echo $HOST-1> /etc/hosts
-echo $HOST-1> /etc/hosts
-echo $HOST-1> /etc/hosts
-echo $HOST-1> /etc/hosts
-echo $HOST-1> /etc/hosts
-echo $HOST-1> /etc/hosts
-echo $HOST-1> /etc/hosts
-echo $HOST-1> /etc/hosts
+echo $HOST-1 >> /etc/hosts            
+echo $HOST-2 >> /etc/hosts
+echo $HOST-3 >> /etc/hosts
+echo $HOST-4 >> /etc/hosts
+echo $HOST-5 >> /etc/hosts
+echo $HOST-6 >> /etc/hosts
+echo $HOST-7 >> /etc/hosts
+echo $HOST-8 >> /etc/hosts
+echo $HOST-9 >> /etc/hosts
+echo $HOST-10 >> /etc/hosts
