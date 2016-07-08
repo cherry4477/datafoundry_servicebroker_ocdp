8b6d825ebf6fcbba4cbee8946432c0d41c52b815
MySQL <871810908@qq.com>
2016-07-08 11:18:36 +0800

    Create host-env.sh

---
+++ host-env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo $HOST-1                 
+echo $HOST-1> /etc/hosts
+echo $HOST-1> /etc/hosts
+echo $HOST-1> /etc/hosts
+echo $HOST-1> /etc/hosts
+echo $HOST-1> /etc/hosts
+echo $HOST-1> /etc/hosts
+echo $HOST-1> /etc/hosts
+echo $HOST-1> /etc/hosts
+echo $HOST-1> /etc/hosts
+echo $HOST-1> /etc/hosts
